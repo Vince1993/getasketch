73333ceff24d4890b694ce2bd07a2cf8db73e788
Vince1993 <52530147+Vince1993@users.noreply.github.com>
2025-11-20 11:43:13 +0100

    changed_folders

---
+++ javaScript.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $("#respo").click(slideInAndOut);
+
+    for (let i = 0; i <= 17; i++) {
+        $('.img_grid').append(`<img src="img_small/sketch${i}.jpg" id="img${i}">`);
+    }
+
+    $(".img_grid img").on("click", function () {
+        if (!document.fullscreenElement) {
+            // Enter fullscreen
+            this.requestFullscreen();
+        } else {
+            // Exit fullscreen
+            document.exitFullscreen();
+        }
+    });
+
+    $("a[href^='#']").click(function (e) {
+        e.preventDefault(); // prevent default jump
+        var target = $($(this).attr("href"));
+        if (target.length) {
+            $("html, body").animate({
+                scrollTop: target.offset().top
+            }, 200); // smooth scroll in 800ms
+        }
+    });
+
+    $("a").on("touchstart", function () {
+        $(this).addClass("link_hover_effect");
+      });
+});
+
+function slideInAndOut() {
+    $("nav a").slideToggle(1000);}
+
+
+
+
